dd8bf753152998f2c3c565ab369acae1d97c59eb
Саша Мєдвєдєва <medvedevasasha09@gmail.com>
2026-04-30 13:43:47 +0300

    лаба6

--- shop/views.py
+++ shop/views.py
@@ -1,42 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
-# Головна сторінка
 def index(request):
     categories = Category.objects.all()
     hits = Product.objects.filter(is_hit=True)
-    context = {
-        'title': 'Fluffy Prostir - Головна',
+    return render(request, 'shop/index.html', {
         'categories': categories,
-        'hits': hits,
-    }
-    return render(request, 'shop/index.html', context)
+        'hits': hits
+    })
 
-# Сторінка "Про нас"
-def about(request):
-    return render(request, 'shop/about.html', {'categories': Category.objects.all()})
+# Нова функція для детальної сторінки товару
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'shop/product_detail.html', {
+        'product': product,
+        'categories': Category.objects.all()
+    })
 
-# Категорія: Парфуми
 def parfume(request):
-    items = Product.objects.filter(category__name__icontains='парфум')
+    items = Product.objects.filter(category__name__icontains='Парфуми')
     return render(request, 'shop/parfume.html', {'items': items, 'categories': Category.objects.all()})
 
-# Категорія: Косметика
 def cosmetics(request):
     items = Product.objects.filter(category__name__icontains='косметика')
     return render(request, 'shop/cosmetics.html', {'items': items, 'categories': Category.objects.all()})
 
-# Категорія: Одяг
 def clothing(request):
     items = Product.objects.filter(category__name__icontains='одяг')
     return render(request, 'shop/clothing.html', {'items': items, 'categories': Category.objects.all()})
 
-# Категорія: Взуття (ЦЕ ВИПРАВИТЬ ТВОЮ ПОМИЛКУ НА СКРИНШОТІ)
 def footwear(request):
     items = Product.objects.filter(category__name__icontains='взуття')
     return render(request, 'shop/footwear.html', {'items': items, 'categories': Category.objects.all()})
 
-# Категорія: Сумки
 def bag(request):
-    items = Product.objects.filter(category__name__icontains='сумки')
+    items = Product.objects.filter(category__name__icontains='сумк')
     return render(request, 'shop/bag.html', {'items': items, 'categories': Category.objects.all()})
+
+def about(request):
+    return render(request, 'shop/about.html', {'categories': Category.objects.all()})
+
